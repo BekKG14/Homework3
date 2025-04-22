23cee242d0cc55319d9f4d6989df747f606d992c
BekKG <nightwing0_14@mail.ru>
2025-04-22 22:21:18 +0300

    HomeWork if done

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //Задача 1
-        int age = 18;
-        if(age >= 18){
-            System.out.println("Человек совершеннолетний");
-        } else {
-            System.out.println("Нужно подождать");
-        }
-        // Задача 2
-        int temperatureOutside = 3;
-        if (temperatureOutside < 5){
-            System.out.printf("На улице %d градусов, нужно надеть шапку", temperatureOutside );
-        } else {
-            System.out.printf("На улице %d, можно ходить без шапки", temperatureOutside);
-        }
+
 
     }
 }
